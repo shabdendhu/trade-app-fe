08d11d70f50e6fe82e0a27864ec3bd14eab8a186
shabdendhu.tripathy <shabdendhus@lecturenotes.in>
2023-09-30 17:37:37 +0530

    d

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -17,7 +17,7 @@ export async function POST(request) {
 
   // Prepare the data for the POST request to Upstox
   const requestData = new URLSearchParams();
-  requestData.append("code", authCode);
+  requestData.append("code", reqBody.code);
   requestData.append("client_id", clientId);
   requestData.append("client_secret", clientSecret);
   requestData.append("redirect_uri", redirectUri);
@@ -27,13 +27,7 @@ export async function POST(request) {
     // Make the POST request to Upstox's token endpoint
     const response = await axios.post(
       "https://api-v2.upstox.com/login/authorization/token",
-      {
-        code: reqBody.code,
-        client_id: "955319ac-7b6f-4565-9556-e5eb30685d9d",
-        client_secret: "q068cmks7h",
-        redirect_uri: "https://trade-app-fe.vercel.app/custom/candel-chart",
-        grant_type: "authorization_code",
-      },
+      requestData,
       {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
@@ -50,7 +44,7 @@ export async function POST(request) {
     // res.status(200).json({ access_token: accessToken });
     return NextResponse.json({ access_token: accessToken }, { status: 200 });
   } catch (error) {
-    console.error("Error exchanging code for token:", error);
+    console.error("Error exchanging code for token:", Object.keys(error));
     return NextResponse.json({ error: error }, { status: 500 });
   }
 }
